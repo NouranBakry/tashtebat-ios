8723e6e59d3fe1ae24cb789a4f5b8677a988bd87
NouranBakry <nouranaymanbakry@gmail.com>
2024-12-20 18:27:12 +0200

    Initial Commit

---
+++ Tashtebat/TashtebatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TashtebatApp.swift
+//  Tashtebat
+//
+//  Created by Nouran Bakry on 20/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TashtebatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
